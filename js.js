d3a8a4be80a2749755a74a439af038e8e21e7d0d
Ryan Crawford <RyanCCrawford@live.com>
2019-04-04 14:18:25 -0400

    Updated the Logic and Styling

--- js.js
+++ js.js
@@ -1,36 +1,59 @@
-$(document).ready(function () {
-    var html = '';
-    var index = -1;
-    const numOfDigits = 9;
-    var holder = [];
-    for (var i = 0; i <= numOfDigits + 1; i++) {
+const digits = 60;
+const HowManyNumbers = 6;
+var holder = [];
 
-        var inner = createBall(random(0, 9));
-        holder.push(inner);
+
+$(document).ready(function () {
+
+    var lotteryNumber = [];
+    var index = -1;
+
+    $('#generate').on('click', go);
+
+    function go() {
+        clearBalls();
+        lotteryNumber = generateLottoNumbers(lotteryNumber);
+        for (var i = 0; i <= lotteryNumber.length - 1; i++) {
+
+            var ball = createBall(lotteryNumber[i]);
+
+            holder.push(ball);
+
+            $('#box').append(ball).delay(1000);
+
+        }
     }
 
-        $('#box').delay(1500).append(holder[0]);
+    function clearBalls() {
+        $('#box').empty();
+        index = -1;
+        lotteryNumber = [];
+    }
 
-   
-        $('#box').delay(1500).append(holder[1]);
+    function generateLottoNumbers(_lotteryNumber) {
 
-        $('#box').delay(1500).append(holder[2]);
+        while (_lotteryNumber.length < HowManyNumbers) {
 
-    
-        $('#box').delay(1500).append(holder[3]);
-
+            var rnd = randomNum(0, digits);
+            if (!_lotteryNumber.includes(rnd)) {
+                _lotteryNumber.push(rnd);
+            }
+        }
+        return _lotteryNumber;
+    }
 
     function createBall(number) {
 
         var ball = $('<span>');
         $(ball).text(number);
         $(ball).addClass('ball');
-        index++
-        $(ball).attr('id', 'b' + index.toString());
+        index++;
+        $(ball).css('animation', 'bounce-in-top ' + (index * 500).toString() + 'ms cubic-bezier(0.645, 0.045, 0.355, 1.000) forwards');
+        $(ball).attr('id', 'ball_' + index.toString());
         return ball;
     }
 
-    function random(min, max) {
+    function randomNum(min, max) {
 
         return Math.floor((Math.random() * max) + min);
 
